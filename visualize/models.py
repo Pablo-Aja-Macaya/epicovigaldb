6d677f19bf701f465ce7600d0b5147d1929445f9
Pablo-Aja-Macaya <paabloaja@gmail.com>
2021-04-05 12:28:44 +0200

    g

--- visualize/models.py
+++ visualize/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from upload.models import Sample, Region
 from upload.models import SampleMetaData
-from tests.models import PicardTest, NextcladeTest, LineagesTest, NGSstatsTest, SingleCheckTest, VariantsTest
+from tests.models import PicardTest, NextcladeTest, NGSstatsTest, SingleCheckTest, VariantsTest
+# from tests.models import LineagesTest
+
 import django_tables2 as tables
 import django_filters
 from django_tables2.utils import A
@@ -37,7 +39,7 @@ class LineagesTable(tables.Table):
     id_uvigo = tables.LinkColumn('specific_sample', args=[A('id_uvigo')])
     date = tables.DateTimeColumn(format ='d/m/Y, h:i A')
     class Meta:
-        model = LineagesTest
+        # model = LineagesTest
         template_name = "table_template.html"
 
 class PicardTable(tables.Table):
