11b0410c148742c3d49f683087428133bb5bdd2b
Pablo-Aja-Macaya <paabloaja@gmail.com>
2021-06-20 21:03:20 +0200

    boton de hacia atras en jobstatus

--- jobstatus/views.py
+++ jobstatus/views.py
@@ -9,6 +9,9 @@ from django_tables2 import RequestConfig
 def check(request):
     table = StatusTable(Status.objects.all().order_by('-fecha'))
     RequestConfig(request).configure(table)
-    table.paginate(page=request.GET.get("page", 1), per_page=50)      
-    context = {'table':table}
+    table.paginate(page=request.GET.get("page", 1), per_page=50) 
+
+    prev_url = request.META.get('HTTP_REFERER')
+
+    context = {'table':table, 'prev_url':prev_url}
     return render(request, 'jobstatus/status.html', context)
