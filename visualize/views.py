4f27878d543e1b0b0ef28d6f06f837aa94bf930f
Pablo-Aja-Macaya <paabloaja@gmail.com>
2021-07-04 04:19:59 +0200

    up

--- visualize/views.py
+++ visualize/views.py
@@ -74,8 +74,7 @@ def get_graphs(request):
     else:
         fecha_inicial = '2020-01-01'
         fecha_final = date.today().strftime('%Y-%m-%d')
-        categoria = 'aleatoria'
-        inicial = {'fecha_inicial':fecha_inicial, 'fecha_final':fecha_final, 'categoria':'aleatoria'}
+        inicial = {'fecha_inicial':fecha_inicial, 'fecha_final':fecha_final}
         
         form = GraphsFormMultipleChoice(initial=inicial)
         # Estado de filtro (colapsado/no colapsado)
