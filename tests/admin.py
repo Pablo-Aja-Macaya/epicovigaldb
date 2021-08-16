13a25a7efcac00516760a1340f06218ebe46f7a2
Pablo-Aja-Macaya <pablo.aja@udc.es>
2021-08-16 13:48:26 +0200

    Export en admin

--- tests/admin.py
+++ tests/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+import csv
+from django.http import HttpResponse
 
 # Register your models here.
 from .models import SingleCheckTest, NGSstatsTest, NextcladeTest
@@ -6,45 +8,64 @@ from .models import LineagesTest#, LineagesMostCommonCountries
 from .models import VariantsTest
 from .models import PicardTest
 
-class PicardTestAdmin(admin.ModelAdmin):
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+class PicardTestAdmin(admin.ModelAdmin, ExportCsvMixin):
     ordering = ['date']
     search_fields = ['id_uvigo_id__id_uvigo']
     list_display = [f.name for f in PicardTest._meta.get_fields()]
-
     list_filter = ('id_uvigo_id__samplemetadata__id_hospital',)
+    actions = ["export_as_csv"]
 
-class NGSstatsTestAdmin(admin.ModelAdmin):
+class NGSstatsTestAdmin(admin.ModelAdmin, ExportCsvMixin):
     ordering = ['date']
     search_fields = ['id_uvigo_id__id_uvigo']
     list_display = [f.name for f in NGSstatsTest._meta.get_fields()]
-
     list_filter = ('id_uvigo_id__samplemetadata__id_hospital',)
+    actions = ["export_as_csv"]
 
-class NextcladeTestAdmin(admin.ModelAdmin):
+class NextcladeTestAdmin(admin.ModelAdmin, ExportCsvMixin):
     ordering = ['date']
     search_fields = ['id_uvigo_id__id_uvigo']
     list_display = [f.name for f in NextcladeTest._meta.get_fields()]
-
     list_filter = ('id_uvigo_id__samplemetadata__id_hospital','clade',
                     'qc_private_mutations_status','qc_missing_data_status','qc_snp_clusters_status',
                     'qc_mixed_sites_status','qc_overall_status','qc_frameshifts_status','qc_stopcodons_status')
+    actions = ["export_as_csv"]
 
 class VariantsTestAdmin(admin.ModelAdmin):
     ordering = ['date']
     search_fields = ['id_uvigo_id__id_uvigo']
     list_display = [f.name for f in VariantsTest._meta.get_fields()]
-
     list_filter = ('id_uvigo_id__samplemetadata__id_hospital',)
+    actions = ["export_as_csv"]
 
-class LineagesTestAdmin(admin.ModelAdmin):
+class LineagesTestAdmin(admin.ModelAdmin, ExportCsvMixin):
     ordering = ['date']
     search_fields = ['id_uvigo_id__id_uvigo']
-    
     l = [f.name for f in LineagesTest._meta.get_fields()]
     l.remove('most_common_countries')
     list_display = l
-
     list_filter = ('id_uvigo_id__samplemetadata__id_hospital','lineage')
+    actions = ["export_as_csv"]
 
 admin.site.register(PicardTest, PicardTestAdmin)
 # admin.site.register(SingleCheckTest)
--- upload/admin.py
+++ upload/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+import csv
+from django.http import HttpResponse
 
 from .models import Region, Sample, SampleMetaData
 
-class RegionAdmin(admin.ModelAdmin):
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+class RegionAdmin(admin.ModelAdmin, ExportCsvMixin):
     ordering = ['localizacion','cp']
     search_fields = ['localizacion','cp','division','pais']
     l = [f.name for f in Region._meta.get_fields()]
@@ -10,17 +31,20 @@ class RegionAdmin(admin.ModelAdmin):
     list_display = l
     list_editable = ['latitud', 'longitud', 'localizacion_org']
     list_filter = ('localizacion',)
+    actions = ["export_as_csv"]
 
-class SampleAdmin(admin.ModelAdmin):
+class SampleAdmin(admin.ModelAdmin, ExportCsvMixin):
     ordering = ['id_uvigo']
     search_fields = ['id_uvigo']
     list_display = [f.name for f in Sample._meta.fields]
     list_filter = ('categoria_muestra','vigilancia','samplemetadata__vacunacion_tipo','samplemetadata__id_hospital')
+    actions = ["export_as_csv"]
 
-class SampleMetaDataAdmin(admin.ModelAdmin):
+class SampleMetaDataAdmin(admin.ModelAdmin, ExportCsvMixin):
     ordering = ['id_uvigo_id__id_uvigo']
     search_fields = ['id_uvigo_id__id_uvigo']
     list_display = [f.name for f in SampleMetaData._meta.fields]
+    actions = ["export_as_csv"]
 
 admin.site.register(Region, RegionAdmin)
 admin.site.register(Sample, SampleAdmin)
